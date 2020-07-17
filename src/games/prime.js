f108e151766fcc77bed301ad2cf1d6be7045b046
Alexey Buravov <night.lord.189@gmail.com>
2020-07-18 02:38:26 +0600

    prime fix - remove unecessary condition

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,9 +7,6 @@ const isPrime = (n) => {
   if (n === 1) {
     return false;
   }
-  if (n === 2) {
-    return true;
-  }
   for (let i = 2; i <= Math.floor(Math.sqrt(n)); i += 1) {
     if (n % i === 0) {
       return false;
